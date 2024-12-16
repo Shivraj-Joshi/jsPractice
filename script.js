f1cd6791a78f5cd3e3f7be017cfed70d3217cdd8
Shivraj joshi <shibujoshi07@gmail.com>
2024-12-16 15:20:03 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const box = document.querySelector(".box")
 const btn = document.querySelector(".btn")
+const addFrnd = document.querySelector('.addFrnd')
+
 
 flag = 0;
+flag2 = 0;
 
 btn.addEventListener("click", () => {
 
@@ -16,4 +19,21 @@ btn.addEventListener("click", () => {
     }
 
 
+})
+
+
+addFrnd.addEventListener("click", () => {
+    if (flag2 == 0) {
+
+        addFrnd.style.backgroundColor = "#C0C0C0"
+        addFrnd.style.col
+        addFrnd.innerHTML = "Cancel request"
+        flag2 = 1;
+    }
+    else {
+        addFrnd.style.backgroundColor = " #1F48C3"
+        addFrnd.style.color = "#fffff"
+        addFrnd.innerHTML = "Add Friend"
+        flag2 = 0;
+    }
 })
