4325e294f3a78dcf67f6b3fe953404a1b483b698
Shivraj joshi <shibujoshi07@gmail.com>
2025-07-07 23:37:42 +0530

    hoisting and scope

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let a = 5;
-const b = 5;
-var c = a + b;
-console.log(c);
+// let a = 5;
+// const b = 5;
+// var c = a + b;
+// console.log(c);
 
-console.log(9 * '3');           
+// console.log(9 * '3');           
+
+//--------------------------------------------------------------------------------------------------------
+// c = 100;
+a = 66;
+
+
+
+let a; // reassigning the variable is allowed. hoisting cannot be done
+// const b;    //can not reassign a constant value
+var c; // reassigning is allowed , hoisting is allowed.
+
+// b = "Lannister always pays its debt"
+
+console.log(a);
+// console.log(b);
+// console.log(c);
