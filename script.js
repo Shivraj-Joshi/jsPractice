31be5dbd0fb4c8022527e819c2c560b249304450
Shivraj joshi <shibujoshi07@gmail.com>
2025-07-21 21:52:59 +0530

    basic to-do list , without styling.

--- script.js
+++ script.js
@@ -1,86 +1,121 @@
-// let a = 5;
-// const b = 5;
-// var c = a + b;
-// console.log(c);
+// // let a = 5;
+// // const b = 5;
+// // var c = a + b;
+// // console.log(c);
 
-// console.log(9 * '3');           
+// // console.log(9 * '3');
 
-//--------------------------------------------------------------------------------------------------------
-// c = 100;
-// a = 66;
-
-
-
-let a; // reassigning the variable is allowed. hoisting cannot be done
-// const b;    //can not reassign a constant value
-var c; // reassigning is allowed , hoisting is allowed.
-
-// b = "Lannister always pays its debt"
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-//----------------------------------------------------------------------------------------------------------------
-
-/* temporal dead zone and variable hoisting */
-
-// {
-//     // TDZ starts at beginning of scope
-//     const func = () => console.log(letVar); // OK
-
-//     // Within the TDZ letVar access throws `ReferenceError`
-
-//     let letVar = 3; // End of TDZ (for letVar)
-//     func();  // Called outside TDZ!
-// }
-
-// --------------------------------------------------------------------------------------------------------------------
-
-// let str = "winter"
-// let str2 = str.split('');
-// console.log(str2);
-// let rev = str2.reverse();
-// console.log(rev);
-
-// -----------------------------------------------------Conditional statement-----------------------------------------------------
+// //--------------------------------------------------------------------------------------------------------
+// // c = 100;
+// // a = 66;
 
 
-// let str = "Nitin"
-// let str2 = str.split('')
 
-// let rev = str2.reverse().join('');
+// let a; // reassigning the variable is allowed. hoisting cannot be done
+// // const b;    //can not reassign a constant value
+// var c; // reassigning is allowed , hoisting is allowed.
 
-// if (str === rev) {
-//     console.log("palindrome")
-// } else {
-//     console.log("Not a palindrome")
-// }
+// // b = "Lannister always pays its debt"
 
-//-----------------------------------------------------conditional loop-------------------------------------------------------------
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
 
-// let i = 1;
+// //----------------------------------------------------------------------------------------------------------------
 
-// for (i; i <= 100; i++) {
-//     if (i % 15 == 0) {
-//         console.log('buzzzFuzzz')
-//     } else if (i % 3 == 0) {
-//         console.log('buzzz')
-//     } else if (i % 5 == 0) {
-//         console.log('Fuzzzz')
-//     } else {
-//         console.log(i);
+// /* temporal dead zone and variable hoisting */
+
+// // {
+// //     // TDZ starts at beginning of scope
+// //     const func = () => console.log(letVar); // OK
+
+// //     // Within the TDZ letVar access throws `ReferenceError`
+
+// //     let letVar = 3; // End of TDZ (for letVar)
+// //     func();  // Called outside TDZ!
+// // }
+
+// // --------------------------------------------------------------------------------------------------------------------
+
+// // let str = "winter"
+// // let str2 = str.split('');
+// // console.log(str2);
+// // let rev = str2.reverse();
+// // console.log(rev);
+
+// // -----------------------------------------------------Conditional statement-----------------------------------------------------
+
+
+// // let str = "Nitin"
+// // let str2 = str.split('')
+
+// // let rev = str2.reverse().join('');
+
+// // if (str === rev) {
+// //     console.log("palindrome")
+// // } else {
+// //     console.log("Not a palindrome")
+// // }
+
+// //-----------------------------------------------------conditional loop-------------------------------------------------------------
+
+// // let i = 1;
+
+// // for (i; i <= 100; i++) {
+// //     if (i % 15 == 0) {
+// //         console.log('buzzzFuzzz')
+// //     } else if (i % 3 == 0) {
+// //         console.log('buzzz')
+// //     } else if (i % 5 == 0) {
+// //         console.log('Fuzzzz')
+// //     } else {
+// //         console.log(i);
+// //     }
+// // };
+
+// //---------------------------------------------------loops-----------------------------------------------------------------
+
+
+// let res = 1;
+// function fact(n) {
+//     for (let i = 1; i <= n; i++) {
+//         res *= i;
 //     }
-// };
+//     return res;
+// }
+// console.log(fact(5))
 
-//---------------------------------------------------loops-----------------------------------------------------------------
+
+const takeInput = document.getElementById('takeInput');
+const addTask = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
 
 
-let res = 1;
-function fact(n) {
-    for (let i = 1; i <= n; i++) {
-        res *= i;
+// const taskText = takeInput.value.trim();
+// console.log(taskText);
+
+
+
+addTask.addEventListener('click', () => {
+    const taskText = takeInput.value.trim();
+
+
+    if (taskText !== '') {
+        const listItem = document.createElement('li');
+        listItem.textContent = taskText;
+
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = ' Delete';
+        deleteBtn.classList.add('delete-btn');
+        listItem.appendChild(deleteBtn);
+
+        taskList.appendChild(listItem);
+        takeInput.value = '';
+
+        deleteBtn.addEventListener('click', () => {
+            taskList.removeChild(listItem);
+        })
+
     }
-    return res;
-}
-console.log(fact(5))
+})
